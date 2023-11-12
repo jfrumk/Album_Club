5667208fd5b248a5b66532a88555d46ab646d986
jfrumk <jafrumkes@gmail.com>
2023-11-12 09:56:55 -0600

    integrate tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx,}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        carbon: {
+          500: "#57504d",
+          600: "#453f3d",
+          700: "#393433",
+          800: "#2a2727",
+          900: "#181716",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
